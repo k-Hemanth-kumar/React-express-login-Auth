ee3a17b352cb133a92bd69c3de5cb0e73b584767
Hemanth <hk0254862@gmail.com>
2024-09-26 16:35:00 +0530

    "added"

--- server.js
+++ server.js
@@ -18,7 +18,3 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
-
-app.listen(5000,()=>{
-    console.log("listening")
-})
