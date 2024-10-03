3a5fc3c3ca9365e4c79377946c3d6712c8cae6da
Hemanth <hk0254862@gmail.com>
2024-10-03 14:37:55 +0530

    database connected successfully

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const path = require('path');
-const cors=require('cors');
+const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 5000;
-const corsOption={
+const corsOption = {
   origin: "http://localhost:5001"
 }
 app.use(cors(corsOption))
@@ -11,26 +11,27 @@ app.use(express.json());
 
 
 // parse requests of content-type - application/x-www-form-urlencoded
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 
 // Serve static files from the React src directory
 app.use(express.static(path.join(__dirname, 'ui/src')));
 
 
 //database handling
-const dbconfig=require('./backend/config/db.config');
-const db=require('./backend/model');
-const Role=db.role;
+const dbconfig = require('./backend/config/db.config');
+const db = require('./backend/model');
+const Role = db.role;
 
-db.mongoose.connect(`mongodb://${dbconfig.HOST}:${dbconfig.PORT}/${dbconfig.DB}`).then(()=>{
+db.mongoose.connect(`mongodb://${dbconfig.HOST}:${dbconfig.PORT}/${dbconfig.DB}`).then(() => {
   console.log("successfully connected to mongodb");
-}).catch((error)=>{
+  initial();
+}).catch((error) => {
   console.error("Connection error", error);
-    process.exit();
+  process.exit();
 })
 
-function initial(){
-  Role.estimatedDocumentCount((error,count)=>{
+async function initial() {
+  /*Role.estimatedDocumentCount((error,count)=>{
     if(!error && count!==0){
       new Role({name:'user'}).save((error)=>{
         if(error){
@@ -52,17 +53,33 @@ function initial(){
         console.log(`added 'moderator' to the role collection`);
       });
     }
-  });
+  });*/
+  try {
+    const count = await Role.estimatedDocumentCount();
+    if (count === 0) {
+
+      await new Role({ name: 'user' }).save();
+      console.log(`added 'user' to the role collection`);
+
+      await new Role({ name: 'admin' }).save();
+      console.log(`added 'admin' to the role collection`);
+
+      await new Role({ name: 'moderator' }).save();
+      console.log(`added 'moderator' to the role collection`);
+    }
+  } catch (error) {
+    console.log("error", error);
+  }
 }
 
 app.get('/', (req, res) => {
-  res.json({message:"Welcome to node example"})
-   //res.sendFile(path.join(__dirname, 'ui/src', 'index.js')); // Change this to index.html
- });
- 
- app.post('/auth/login',(req,res)=>{
-   console.log(req.body,req.url,req.baseUrl);
- })
+  res.json({ message: "Welcome to node example" })
+  //res.sendFile(path.join(__dirname, 'ui/src', 'index.js')); // Change this to index.html
+});
+
+app.post('/auth/login', (req, res) => {
+  console.log(req.body, req.url, req.baseUrl);
+})
 
 // Start the server
 app.listen(PORT, () => {
